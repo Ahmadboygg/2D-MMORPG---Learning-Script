2dafb4b34abe38c88d997f19a8b0ef0e47e534f3
Ahmadboygg <97325312+Ahmadboygg@users.noreply.github.com>
2023-01-23 17:35:26 +0700

    ActionMenuPosition
    Script untuk mengatur dan memanggil objek action menu di posisi yang telah di tentukan

---
+++ ActionMenuPosition.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMenuPosition : MonoBehaviour
+{
+    [SerializeField] private GameObject actionMenu;
+    [SerializeField] private RectTransform[] actionMenuPoint; // array for action menu position
+    public int currentActiveMenu;
+    void Start()
+    {
+        if(actionMenu != null || actionMenuPoint != null){
+            foreach(RectTransform actionMenuPoints in actionMenuPoint){
+                Instantiate(actionMenu,actionMenuPoints);
+                actionMenuPoints.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    void Update()
+    {
+        for(int i = 0; i < actionMenuPoint.Length; i++)
+        {
+            actionMenuPoint[i].gameObject.SetActive(i == currentActiveMenu);
+        }
+    }
+
+    
+}
