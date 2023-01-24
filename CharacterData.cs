36436977deb49c9b28bf039ba351e51371f90000
Ahmadboygg <97325312+Ahmadboygg@users.noreply.github.com>
2023-01-24 22:37:51 +0700

    CharacterData - ScriptableObject

---
+++ CharacterData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName ="Create Character Data", fileName ="New Character Data")]
+public class CharacterData : ScriptableObject
+{
+
+    public new string name;
+
+    public int maxHP;
+    public int maxMP;
+    public int maxSP;
+
+    public GameObject battler;
+}
